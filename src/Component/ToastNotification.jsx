0e0cf3a8ef40658122597c7b4a7b46312d23601f
shishirRsiam <shishir.siam01@gmail.com>
2025-01-19 04:33:27 +0600

    project done. all fixed code Deploy Files.

--- src/Component/ToastNotification.jsx
+++ src/Component/ToastNotification.jsx
@@ -12,12 +12,10 @@ const ExampleComponent = () => {
 
     return (
         <div>
-            <button onClick={showSuccessNotification}
-                className="bg-green-500 text-white py-2 px-4 rounded-lg">
+            <button onClick={showSuccessNotification} className="bg-green-500 text-white py-2 px-4 rounded-lg">
                 Show Success Notification
             </button>
-            <button onClick={showErrorNotification}
-                className="bg-red-500 text-white py-2 px-4 rounded-lg ml-4">
+            <button onClick={showErrorNotification} className="bg-red-500 text-white py-2 px-4 rounded-lg ml-4">
                 Show Error Notification
             </button>
         </div>
